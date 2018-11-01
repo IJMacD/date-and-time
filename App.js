9d232ae044dcff3915ea8af0f228df98ab8eafeb
Iain MacDonald <IJMacD@gmail.com>
2018-11-01 22:56:31 +0800

    Move SunGraph into its own component

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import moment from 'moment';
-import Expo, { Svg } from 'expo';
-import suncalc from 'suncalc';
-import ProgressCircle from 'react-native-progress-circle'
+import Expo from 'expo';
+import SunCalc from 'suncalc';
+import ProgressCircle from 'react-native-progress-circle';
+import SunGraph from './SunGraph';
+
 const { Localization } = Expo.DangerZone;
 
 export default class App extends React.Component {
@@ -46,7 +48,7 @@ export default class App extends React.Component {
     let mf, mp;
     if (this.state.loc) {
       const today = d.toDate();
-      const moon = suncalc.getMoonIllumination(today);
+      const moon = SunCalc.getMoonIllumination(today);
       mf = moon.fraction;
       mp = moon.phase;
     }
@@ -66,7 +68,7 @@ export default class App extends React.Component {
         <View style={styles.sun}>
           <Text style={styles.sunLabel}>Sun</Text>
           <View style={{display:"flex", alignItems:"center"}}>
-            <SunGraph date={new Date(this.state.now)} loc={this.state.loc} />
+            <SunGraph date={new Date(this.state.now)} location={this.state.loc} />
           </View>
         </View>
         <View style={styles.moon}>
@@ -163,124 +165,3 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
-function dateToX (date, width) {
-  return (date.getHours() + date.getMinutes() / 60) / 24 * width;
-}
-
-function toDeg (r) {
-  return r / Math.PI * 180;
-}
-
-function SunGraph (props) {
-  const width = 300;
-  const height = 80;
-
-  if (!props.loc) {
-    return null;
-  }
-
-  const { latitude, longitude } = props.loc.coords;
-  const { sunrise, sunset, solarNoon } = suncalc.getTimes(props.date, latitude, longitude);
-  const sr = moment(sunrise);
-  const ss = moment(sunset);
-  const sn = moment(solarNoon);
-  const dl = moment.duration(ss.diff(sr));
-  const sp = suncalc.getPosition(props.date, latitude, longitude);
-  const srp = suncalc.getPosition(sunrise, latitude, longitude);
-  const ssp = suncalc.getPosition(sunset, latitude, longitude);
-  const snp = suncalc.getPosition(solarNoon, latitude, longitude);
-
-  const srx = dateToX(sr.toDate(), width);
-  const ssx = dateToX(ss.toDate(), width);
-  const snx = dateToX(sn.toDate(), width);
-
-  const sHeight = snp.altitude / (Math.PI / 2) * height;
-  const sOffset = height - sHeight;
-
-  return (
-    <Svg
-      width={width}
-      height={height}
-    >
-      <Svg.Path
-        id="horizon"
-        d={`M 0 ${height/2} H ${width}`}
-        stroke="#999"
-        fillOpacity="0"
-      />
-      <Svg.Path
-        id="sunpath"
-        d={`M 0 ${height-sOffset/2} C ${width/4} ${height-sOffset/2} ${width/4} ${sOffset/2} ${width/2} ${sOffset/2} C ${width*3/4} ${sOffset/2} ${width*3/4} ${height-sOffset/2} ${width} ${height-sOffset/2}`}
-        stroke="#88f"
-        fillOpacity="0"
-      />
-      <Svg.Path
-        id="daylength-line"
-        d={`M ${srx} ${height - 4} v 4 H ${ssx} v -4`}
-        stroke="#f80"
-        fillOpacity={0}
-      />
-      <Svg.Text
-        id="sunrise-text"
-        x={srx - 16}
-        y={height/2 + 12}
-        fill="#f80"
-      >
-        {sr.format("HH:mm")}
-        <Svg.TSpan
-          x={srx - 12}
-          dy="1.2em"
-          fill="#f80"
-        >{Math.round(toDeg(srp.azimuth))}°</Svg.TSpan>
-      </Svg.Text>
-      <Svg.Text
-        id="sunset-text"
-        x={ssx - 16}
-        y={height/2 + 12}
-        fill="#f80"
-      >
-        {ss.format("HH:mm")}
-        <Svg.TSpan
-          x={ssx - 12}
-          dy="1.2em"
-          fill="#f80"
-        >{Math.round(toDeg(ssp.azimuth))}°</Svg.TSpan>
-      </Svg.Text>
-      <Svg.Text
-        id="solarnoon-text"
-        x={snx - 16}
-        y={height/6}
-        fill="#f80"
-      >
-        {sn.format("HH:mm")}
-        <Svg.TSpan
-          x={snx - 40}
-          fill="#f80"
-        >{Math.round(toDeg(snp.altitude))}°</Svg.TSpan>
-        <Svg.TSpan
-          x={snx - 12}
-          dy="1.2em"
-          fill="#f80"
-        >{Math.round(toDeg(snp.azimuth))}°</Svg.TSpan>
-      </Svg.Text>
-      <Svg.Text
-        id="daylength-text"
-        x={width / 2 - 30}
-        y={height - 1}
-        fill="#f80"
-      >{`${dl.hours()}:${padStart(dl.minutes())}:${padStart(dl.seconds())}`}</Svg.Text>
-      <Svg.Circle
-        id="sun"
-        cx={dateToX(props.date, width)}
-        cy={(height / 2)-sp.altitude/(Math.PI/2)*(height/2)}
-        r={5}
-        fill="#f80"
-      />
-    </Svg>
-  );
-}
-
-function padStart (n, c="0") {
-  return n < 10 ? c + n : n;
-}
---
+++ SunGraph.js
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import { Svg } from 'expo';
+import SunCalc from 'suncalc';
+import moment from 'moment';
+
+function dateToX (date, width) {
+  return (date.getHours() + date.getMinutes() / 60) / 24 * width;
+}
+
+function altToY (altitude, height) {
+  return (height / 2)-altitude/(Math.PI/2)*(height/2);
+}
+
+function toDeg (r) {
+  return r / Math.PI * 180;
+}
+
+/**
+ * Props for SunGraph
+ * @typedef {Object} SunGraphProps
+ * @prop {Date} date
+ * @prop {{latitude: number, longitude: number}} location
+ */
+
+export default class SunGraph extends Component {
+
+  shouldComponentUpdate (nextProps) {
+    if (!this.props.location) {
+      return false;
+    }
+
+    if (!this.props.location && nextProps.location) {
+      return true;
+    }
+
+    const m0 = moment(this.props.date);
+    const m1 = moment(nextProps.date);
+    return !m0.isSame(m1, 'minute');
+  }
+
+  render () {
+    const width = 300;
+    const height = 80;
+
+    if (!this.props.location) {
+      return null;
+    }
+
+    const noon = new Date(this.props.date);
+    noon.setHours(12, 0, 0);
+
+    const { latitude, longitude } = this.props.location.coords;
+    const { sunrise, sunset, solarNoon } = SunCalc.getTimes(noon, latitude, longitude);
+    const sr = moment(sunrise);
+    const ss = moment(sunset);
+    const sn = moment(solarNoon);
+    const dl = moment.duration(ss.diff(sr));
+    const sp = SunCalc.getPosition(this.props.date, latitude, longitude);
+    const srp = SunCalc.getPosition(sunrise, latitude, longitude);
+    const ssp = SunCalc.getPosition(sunset, latitude, longitude);
+    const snp = SunCalc.getPosition(solarNoon, latitude, longitude);
+
+    const srx = dateToX(sr.toDate(), width);
+    const ssx = dateToX(ss.toDate(), width);
+    const snx = dateToX(sn.toDate(), width);
+
+    const sHeight = snp.altitude / (Math.PI / 2) * height;
+    const sOffset = height - sHeight;
+
+    const sps = [];
+    const d = new Date(this.props.date);
+    d.setHours(0, 0, 0);
+    for(let i = 0; i <= 24; i++) {
+      d.setHours(i);
+      const p = SunCalc.getPosition(d, latitude, longitude);
+      const y = altToY(p.altitude, height);
+
+      if (i == 0) {
+        sps.push("M 0 " + y);
+      } else if (i == 24) {
+        sps.push(`L ${width} ${y}`);
+      } else {
+        sps.push(`L ${(i/24)*width} ${y}`);
+      }
+    }
+
+    const solarPath = sps.join(" ");
+
+    return (
+      <Svg
+        width={width}
+        height={height}
+      >
+        <Svg.Path
+          id="horizon"
+          d={`M 0 ${height/2} H ${width}`}
+          stroke="#999"
+          fillOpacity="0"
+        />
+        <Svg.Path
+          id="sunpath"
+          d={solarPath}
+          stroke="#88f"
+          fillOpacity="0"
+        />
+        <Svg.Path
+          id="daylength-line"
+          d={`M ${srx} ${height - 4} v 4 H ${ssx} v -4`}
+          stroke="#f80"
+          fillOpacity={0}
+        />
+        <Svg.Text
+          id="sunrise-text"
+          x={srx - 16}
+          y={height/2 + 12}
+          fill="#f80"
+        >
+          {sr.format("HH:mm")}
+          <Svg.TSpan
+            x={srx - 12}
+            dy="1.2em"
+            fill="#f80"
+          >{Math.round(toDeg(srp.azimuth+Math.PI))}°</Svg.TSpan>
+        </Svg.Text>
+        <Svg.Text
+          id="sunset-text"
+          x={ssx - 16}
+          y={height/2 + 12}
+          fill="#f80"
+        >
+          {ss.format("HH:mm")}
+          <Svg.TSpan
+            x={ssx - 12}
+            dy="1.2em"
+            fill="#f80"
+          >{Math.round(toDeg(ssp.azimuth+Math.PI))}°</Svg.TSpan>
+        </Svg.Text>
+        <Svg.Text
+          id="solarnoon-text"
+          x={snx - 16}
+          y={height/6}
+          fill="#f80"
+        >
+          {sn.format("HH:mm")}
+          <Svg.TSpan
+            x={snx - 40}
+            fill="#f80"
+          >{Math.round(toDeg(snp.altitude))}°</Svg.TSpan>
+          <Svg.TSpan
+            x={snx - 12}
+            dy="1.2em"
+            fill="#f80"
+          >{Math.round(toDeg(snp.azimuth+Math.PI))}°</Svg.TSpan>
+        </Svg.Text>
+        <Svg.Text
+          id="daylength-text"
+          x={width / 2 - 30}
+          y={height - 1}
+          fill="#f80"
+        >{`${dl.hours()}:${padStart(dl.minutes())}:${padStart(dl.seconds())}`}</Svg.Text>
+        <Svg.Circle
+          id="sun"
+          cx={dateToX(this.props.date, width)}
+          cy={altToY(sp.altitude, height)}
+          r={5}
+          fill="#f80"
+        />
+      </Svg>
+    );
+  }
+}
+
+function padStart (n, c="0") {
+  return n < 10 ? c + n : n;
+}
